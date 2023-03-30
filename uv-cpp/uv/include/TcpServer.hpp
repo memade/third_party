79ff7d5695a0ff5f5f6ad972668f69cae89a2235
memade <k34ubll5@outlook.com>
2023-03-30 11:37:52 +0800

    init

---
+++ uv-cpp/uv/include/TcpServer.hpp
@@ -0,0 +1,77 @@
+﻿/*
+   Copyright © 2017-2019, orcaer@yeah.net  All rights reserved.
+
+   Author: orcaer@yeah.net
+
+   Last modified: 2019-12-31
+
+   Description: https://github.com/wlgq2/uv-cpp
+*/
+
+#ifndef UV_TCP_SERVER_HPP
+#define UV_TCP_SERVER_HPP
+
+#include <functional>
+#include <memory>
+#include <set>
+#include <map>
+
+#include "TcpAcceptor.hpp"
+#include "TcpConnection.hpp"
+#include "TimerWheel.hpp"
+
+namespace uv {
+
+ using OnConnectionStatusCallback = std::function<void(std::weak_ptr<TcpConnection>)>;
+
+ //no thread safe.
+ class TcpServer {
+ protected:
+  std::shared_ptr<std::mutex> m_Mutex = std::make_shared<std::mutex>();
+ public:
+  static void SetBufferMode(uv::GlobalConfig::BufferMode mode);
+ public:
+  TcpServer(EventLoop* loop, bool tcpNoDelay = true);
+  virtual ~TcpServer();
+ public:
+  virtual int bindAndListen(SocketAddr& addr);
+
+  virtual void close(DefaultCallback callback);
+
+  virtual TcpConnectionPtr getConnection(const std::string& name);
+  virtual void closeConnection(const std::string& name);
+
+  virtual void setNewConnectCallback(OnConnectionStatusCallback callback);
+  virtual void setConnectCloseCallback(OnConnectionStatusCallback callback);
+
+  virtual void setMessageCallback(OnMessageCallback callback);
+
+  virtual void write(TcpConnectionPtr connection, const char* buf, unsigned int size, AfterWriteCallback callback = nullptr);
+  virtual void write(std::string& name, const char* buf, unsigned int size, AfterWriteCallback callback = nullptr);
+  virtual void writeInLoop(TcpConnectionPtr connection, const char* buf, unsigned int size, AfterWriteCallback callback);
+  virtual void writeInLoop(std::string& name, const char* buf, unsigned int size, AfterWriteCallback callback);
+
+  virtual void setTimeout(unsigned int);
+ protected:
+  virtual void onAccept(EventLoop* loop, UVTcpPtr client);
+
+  virtual void addConnection(std::string& name, TcpConnectionPtr connection);
+  virtual void removeConnection(std::string& name);
+  virtual void onMessage(TcpConnectionPtr connection, const char* buf, ssize_t size);
+ protected:
+  EventLoop* loop_;
+ protected:
+  bool tcpNoDelay_;
+  SocketAddr::IPV ipv_;
+  std::shared_ptr <TcpAcceptor> accetper_;
+  std::map<std::string, TcpConnectionPtr>  connnections_;
+
+  OnMessageCallback onMessageCallback_;
+  OnConnectionStatusCallback onNewConnectCallback_;
+  OnConnectionStatusCallback onConnectCloseCallback_;
+  TimerWheel<ConnectionWrapper> timerWheel_;
+ };
+
+
+}///namespace uv
+#endif
---
+++ uv-cpp/uv/include/http/HttpClient.hpp
@@ -0,0 +1,56 @@
+﻿/*
+   Copyright © 2017-2020, orcaer@yeah.net  All rights reserved.
+
+   Author: orcaer@yeah.net
+
+   Last modified: 2020-3-30
+
+   Description: https://github.com/wlgq2/uv-cpp
+*/
+
+#ifndef UV_HTTP_CLIENT_HPP
+#define UV_HTTP_CLIENT_HPP
+
+#include "../TcpClient.hpp"
+#include "Request.hpp"
+#include "Response.hpp"
+
+namespace uv
+{
+ namespace http
+ {
+
+  class HttpClient
+  {
+  public:
+   enum ReqResult
+   {
+    Success = 0,
+    ConnectFail = 1,
+    ParseFail = 2,
+    Unknow = 3,
+   };
+   using OnRespCallback = std::function<void(ReqResult, Response*)>;
+  public:
+   HttpClient(EventLoop* loop);
+   virtual ~HttpClient();
+
+   virtual void Req(uv::SocketAddr& addr, Request& req);
+   virtual void setOnResp(OnRespCallback callback);
+
+  protected:
+   TcpClient* client_;
+   OnRespCallback callback_;
+   Request req_;
+   std::string buffer_;
+
+  protected:
+   void onResp(ReqResult rst, Response* resp);
+   void onConnectStatus(TcpClient::ConnectStatus status);
+   void onMessage(const char* data, ssize_t size);
+   bool isConnected;
+  };
+
+ }
+}
+#endif
